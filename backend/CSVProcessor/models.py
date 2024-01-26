7b7acdc6dc7ed636ca9e90ff6c6dae9c89245051
mahmoud21a <mahmouderradi23@gmail.com>
2024-01-26 18:10:11 +0100

    removed uncessary code

--- backend/CSVProcessor/models.py
+++ backend/CSVProcessor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    transactionDate = models.DateField()
+    description = models.CharField(max_length=200)
+    saleAmount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.description
